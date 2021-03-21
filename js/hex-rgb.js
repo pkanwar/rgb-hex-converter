f06feed57da74706c2083e2ff725c1d04589928b
prateek <sg_021@rediffmail.com>
2021-03-21 20:51:41 +0530

    changes to make hex-rgb responsive

--- js/hex-rgb.js
+++ js/hex-rgb.js
@@ -6,15 +6,13 @@ const outputSection = document.querySelector('.outputSection');
 const rgbSection = document.querySelector('.rgbTitle');
 const container = document.querySelector('.container');
 
-
-function getRGBValue(hex){
+function getRGBValue(hex) {
     // eslint-disable-next-line radix
     const rgb = parseInt(hex, 16);
     return rgb;
 }
 
-function isHexadecimal(str)
-{
+function isHexadecimal(str) {
     const regexp = /^[0-9a-fA-F]+$/;
     if (regexp.test(str)) {
         return true;
@@ -23,28 +21,26 @@ function isHexadecimal(str)
 }
 
 hexToRgb.addEventListener('click', () => {
-
     const hexString = hexInput.value;
-    if ((hexInput.value.length < 6) || !isHexadecimal(hexString)) {
+    if (hexInput.value.length < 6 || !isHexadecimal(hexString)) {
         // eslint-disable-next-line no-alert
         alert(
             'Please enter valid input: A 6 character hex alphanumeric value that translates to an RGB value within the range of 0-255 for each color',
         );
+    } else {
+        const redString = hexString.substring(0, 2);
+        const greenString = hexString.substring(2, 4);
+        const blueString = hexString.substring(4, 6);
+        const redValue = getRGBValue(redString);
+        const greenValue = getRGBValue(greenString);
+        const blueValue = getRGBValue(blueString);
+
+        output.style.backgroundColor = `#${hexString}`;
+        output.style.border = '2px solid black';
+        outputSection.style.display = 'block';
+        container.style.height = '700px';
+        rgbSection.innerHTML = '(Red : ' + redValue + ', Green : ' + greenValue + ', Blue : ' + blueValue + ')';
     }
-  
-    const redString = hexString.substring(0, 2);
-    const greenString = hexString.substring(2, 4);
-    const blueString = hexString.substring(4, 6);
-    const redValue = getRGBValue(redString);
-    const greenValue = getRGBValue(greenString);
-    const blueValue = getRGBValue(blueString);
-
-    output.style.backgroundColor = `#${hexString}`;
-    output.style.border = '2px solid black';
-    outputSection.style.display = 'block';
-    container.style.height = '700px';
-    rgbSection.innerHTML = '(Red : ' +redValue+ ', Green : ' +greenValue+ ', Blue : '+ blueValue +')';
-
 });
 
 clearBtn.addEventListener('click', () => {
