1c8b87cb4157fc05dd3dd15283224a87f3a76b58
prateek <sg_021@rediffmail.com>
2021-03-21 19:26:44 +0530

    changes to make rgb-hex responsive

--- js/rgb-hex.js
+++ js/rgb-hex.js
@@ -8,6 +8,7 @@ const outputSection = document.querySelector('.outputSection');
 const hexSection = document.querySelector('.hexTitle');
 const container = document.querySelector('.container');
 
+
 function componentToHex(c) {
     const hex = c.toString(16);
     return hex.length == 1 ? `0${hex}` : hex;
@@ -17,7 +18,7 @@ function rgbToHexConvert(r, g, b) {
     return `#${componentToHex(r)}${componentToHex(g)}${componentToHex(b)}`;
 }
 
-function getRGBValue(hex){
+function getRGBValue(hex) {
     // eslint-disable-next-line radix
     const rgb = parseInt(hex, 16);
     return rgb;
@@ -27,43 +28,44 @@ rgbToHex.addEventListener('click', () => {
     if (redInput.value.length === 0 || greenInput.value.length === 0 || blueInput.value.length === 0) {
         // eslint-disable-next-line no-alert
         alert('please enter red, blue, green values');
+    } else {
+        // eslint-disable-next-line radix
+        const redValue = parseInt(redInput.value);
+        // eslint-disable-next-line radix
+        const greenValue = parseInt(greenInput.value);
+        // eslint-disable-next-line radix
+        const blueValue = parseInt(blueInput.value);
+        // eslint-disable-next-line radix
+        if (
+            // eslint-disable-next-line radix
+            parseInt(redValue) > 255 ||
+            // eslint-disable-next-line radix
+            parseInt(redValue) < 0 ||
+            // eslint-disable-next-line radix
+            parseInt(greenValue) > 255 ||
+            // eslint-disable-next-line radix
+            parseInt(greenValue) < 0 ||
+            // eslint-disable-next-line radix
+            parseInt(blueValue) > 255 ||
+            // eslint-disable-next-line radix
+            parseInt(blueValue) < 0
+        ) {
+            // eslint-disable-next-line no-alert
+            alert('please enter value between 0 and 255');
+        } else {
+            let containerHeight = container.offsetHeight;
+            let containerWidth = container.offsetHeight;
+            containerHeight += 150;
+            const color = rgbToHexConvert(redValue, greenValue, blueValue);
+            // eslint-disable-next-line no-console
+            console.log(color);
+            output.style.backgroundColor = color;
+            output.style.border = '2px solid black';
+            outputSection.style.display = 'block';
+            hexSection.innerHTML = color;
+            container.style.height = '900px';
+        }
     }
-    // eslint-disable-next-line radix
-    const redValue = parseInt(redInput.value);
-    // eslint-disable-next-line radix
-    const greenValue = parseInt(greenInput.value);
-    // eslint-disable-next-line radix
-    const blueValue = parseInt(blueInput.value);
-    // eslint-disable-next-line radix
-    if (
-        // eslint-disable-next-line radix
-        parseInt(redValue) > 255 ||
-        // eslint-disable-next-line radix
-        parseInt(redValue) < 0 ||
-        // eslint-disable-next-line radix
-        parseInt(greenValue) > 255 ||
-        // eslint-disable-next-line radix
-        parseInt(greenValue) < 0 ||
-        // eslint-disable-next-line radix
-        parseInt(blueValue) > 255 ||
-        // eslint-disable-next-line radix
-        parseInt(blueValue) < 0
-    ) {
-        // eslint-disable-next-line no-alert
-        alert('please enter value between 0 and 255');
-    }
-
-    const color = rgbToHexConvert(redValue, greenValue, blueValue);
-    // eslint-disable-next-line no-console
-    console.log(color);
-    output.style.backgroundColor = color;
-    output.style.border = '2px solid black';
-    outputSection.style.display = 'block';
-    container.style.height = '900px';
-    hexSection.innerHTML = color;
-
-    console.log("getRGBValue : " + getRGBValue('21'));
-
 });
 
 clearBtn.addEventListener('click', () => {
@@ -72,5 +74,6 @@ clearBtn.addEventListener('click', () => {
     blueInput.value = '';
     output.value = '';
     outputSection.style.display = 'none';
+    let containerWidth = container.offsetHeight;
     container.style.height = '750px';
 });
